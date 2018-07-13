954085ccad8e64f980ada1dbca5ec81932bd4ee2
johnathanbostrom <orrosta@gmail.com>
2018-07-13 08:01:17 -0500

    step now handles duplicate elements

--- elm/SelectList/Extra.elm
+++ elm/SelectList/Extra.elm
@@ -1,53 +1,68 @@
-module SelectList.Extra exposing 
-    ( step
-    , fromList
-    , selectAt
-    , StepDirection(Forwards,Backwards)
-    )
+module SelectList.Extra
+    exposing
+        ( step
+        , fromList
+        , selectAt
+        , StepDirection(Forwards, Backwards)
+        )
 
 {-| A library of SelectList helpers.
 
+
 # Helpers
+
 @docs step, fromList, selectAt
 
+
 # Types
+
 @docs StepDirection
+
 -}
 
 import SelectList exposing (SelectList)
 import List.Extra exposing (splitWhen, getAt, splitAt)
 
+
 {-| Step your selection forwards (increment by one) or backwards (decrement by one) through your list.
 If there are no more elements in the step direction, returns an unmodified select list.
 
     sList = fromLists ["a", "b", "c"] "d" []
-    
+
     step Forwards sList -- ["a", "b", "c"] "d" []
     step Backwards sList  -- ["a", "b"] "c" ["d"]
- -}
+
+-}
 step : StepDirection -> SelectList a -> SelectList a
 step direction sList =
     let
-        newSelected =
-            case direction of
-                Forwards ->
-                    List.head (SelectList.after sList)
-
-                Backwards ->
-                    List.Extra.last (SelectList.before sList)
-
-        newSelected_ =
-            Maybe.withDefault (SelectList.selected sList) newSelected
+        currentIndex =
+            List.length <| SelectList.before sList
     in
-        SelectList.select ((==) newSelected_) sList
+        case direction of
+            Forwards ->
+                selectAt (currentIndex + 1) sList
+
+            Backwards ->
+                selectAt (currentIndex - 1) sList
+
 
 {-| Build a select list from a single list and a selected element.
-If the provided element is not in the list, returns Nothing. 
+If the provided element is not in the list, returns Nothing.
 
-    sList = fromList "b" ["a", "b", "c"] --["a"] "b" ["c"]
-    sList = fromList "z" ["a", "b", "c"] --Nothing
+    sList =
+        fromList "b" [ "a", "b", "c" ]
 
- -}
+
+    --["a"] "b" ["c"]
+
+    sList =
+        fromList "z" [ "a", "b", "c" ]
+
+
+    --Nothing
+
+-}
 fromList : a -> List a -> Maybe (SelectList a)
 fromList selected lst =
     case splitWhen ((==) selected) lst of
@@ -57,37 +72,45 @@ fromList selected lst =
                     before
                     selected
                     (List.drop 1 after)
+
         Nothing ->
             Nothing
 
 
-{-| Select the element at the given index. 
+{-| Select the element at the given index.
 returns unchaged select list if index is invalid.
 
     sList = fromLists ["a", "b", "c"] "d" []
 
     selectAt -1 sList  -- ["a", "b", "c"] "d" []
     selectAt 1 sList  -- ["a"] "b" ["c", "d"]
- -}
+
+-}
 selectAt : Int -> SelectList a -> SelectList a
 selectAt index sLst =
     let
-        lst = SelectList.toList sLst
-        selected = getAt index lst
+        lst =
+            SelectList.toList sLst
+
+        selected =
+            getAt index lst
     in
         case selected of
             Just selected_ ->
                 let
-                    (before, after) = splitAt index lst
+                    ( before, after ) =
+                        splitAt index lst
                 in
                     SelectList.fromLists
                         before
                         selected_
                         (List.drop 1 after)
+
             Nothing ->
                 sLst
 
-{-| Step direction used with step function. 
+
+{-| Step direction used with step function.
 -}
 type StepDirection
     = Forwards
--- elm/tests/Tests.elm
+++ elm/tests/Tests.elm
@@ -1,4 +1,5 @@
 module Tests exposing (..)
+
 import SelectList exposing (SelectList, fromLists)
 import SelectList.Extra exposing (step, StepDirection(..), fromList, selectAt)
 import Expect exposing (Expectation)
@@ -6,93 +7,128 @@ import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
 
-
 stepTests : Test
 stepTests =
     let
-        sList = SelectList.fromLists ["a", "b", "c"] "d" ["e"]
+        sList =
+            fromLists [ "a", "b", "c" ] "d" [ "e" ]
+
+        dupSList =
+            fromLists [ "e", "b", "c" ] "d" [ "e" ]
     in
-        
-    describe "The step function"
-        [ test "step forward" <| 
-            \_ ->
-                let
-                    newSList = step Forwards sList
-                    expected = fromLists ["a", "b", "c", "d"] "e" []
-                in
-                    Expect.equal newSList expected
-        , test "step forward into invalid state returns same sList" <|
-            \_ ->
-                let
-                    newSList = step Forwards <| step Forwards sList
-                    expected = fromLists ["a", "b", "c", "d"] "e" []
-                in
-                    Expect.equal newSList expected
-        , test "step backward" <|
-            \_ ->
-                let
-                    newSList = step Backwards sList
-                    expected = fromLists ["a", "b"] "c" ["d", "e"]
-                in 
-                    Expect.equal newSList expected
-        , test "step backward into invalid state returns same sList" <|
-            \_ ->
-                let
-                    newSList = step Backwards <| step Backwards <| step Backwards <| step Backwards <| sList
-                    expected = fromLists [] "a" ["b", "c", "d", "e"]
-                in
-                    Expect.equal newSList expected                
-        ]
+        describe "The step function"
+            [ test "step forward" <|
+                \_ ->
+                    let
+                        newSList =
+                            step Forwards sList
+
+                        expected =
+                            fromLists [ "a", "b", "c", "d" ] "e" []
+                    in
+                        Expect.equal newSList expected
+            , test "step forward into invalid state returns same sList" <|
+                \_ ->
+                    let
+                        newSList =
+                            step Forwards <| step Forwards sList
+
+                        expected =
+                            fromLists [ "a", "b", "c", "d" ] "e" []
+                    in
+                        Expect.equal newSList expected
+            , test "step backward" <|
+                \_ ->
+                    let
+                        newSList =
+                            step Backwards sList
+
+                        expected =
+                            fromLists [ "a", "b" ] "c" [ "d", "e" ]
+                    in
+                        Expect.equal newSList expected
+            , test "step backward into invalid state returns same sList" <|
+                \_ ->
+                    let
+                        newSList =
+                            step Backwards <| step Backwards <| step Backwards <| step Backwards <| sList
+
+                        expected =
+                            fromLists [] "a" [ "b", "c", "d", "e" ]
+                    in
+                        Expect.equal newSList expected
+            , test "step with duplicates" <|
+                \_ ->
+                    let
+                        newSList =
+                            step Forwards dupSList
+
+                        expected =
+                            fromLists [ "e", "b", "c", "d" ] "e" []
+                    in
+                        Expect.equal newSList expected
+            ]
+
 
 fromListTests : Test
-fromListTests = 
+fromListTests =
     let
-        lst = ["a", "b", "c", "d"]
+        lst =
+            [ "a", "b", "c", "d" ]
     in
         describe "The fromList function"
-            [ mkFromListTest "middle element" "b" lst <| Just <| fromLists ["a"] "b" ["c", "d"] 
-            , mkFromListTest "first element" "a" lst <| Just <| fromLists [] "a" ["b", "c", "d"]
-            , mkFromListTest "last element" "d" lst <| Just <| fromLists ["a", "b", "c"] "d" []
+            [ mkFromListTest "middle element" "b" lst <| Just <| fromLists [ "a" ] "b" [ "c", "d" ]
+            , mkFromListTest "first element" "a" lst <| Just <| fromLists [] "a" [ "b", "c", "d" ]
+            , mkFromListTest "last element" "d" lst <| Just <| fromLists [ "a", "b", "c" ] "d" []
             , mkFromListTest "non extant element returns nothing " "z" lst Nothing
-            , mkFromListTest "singleton" "a" ["a"] <| Just <| fromLists [] "a" []
+            , mkFromListTest "singleton" "a" [ "a" ] <| Just <| fromLists [] "a" []
             ]
 
 
 mkFromListTest : String -> a -> List a -> Maybe (SelectList a) -> Test
 mkFromListTest description element list expected =
     test description <|
-                    \_ ->
-                        let
-                            sList = fromList element list
-                        in
-                            Expect.equal sList expected
+        \_ ->
+            let
+                sList =
+                    fromList element list
+            in
+                Expect.equal sList expected
 
 
 selectAtTests : Test
 selectAtTests =
     let
-        sList = fromLists ["a", "b", "c"] "d" ["e"]
-        singleton = fromLists [] "a" []
-        startSelected = fromLists [] "a" ["b", "c", "d", "e"]
-        endSelected = fromLists ["a", "b", "c", "d"] "e" []
+        sList =
+            fromLists [ "a", "b", "c" ] "d" [ "e" ]
+
+        singleton =
+            fromLists [] "a" []
+
+        startSelected =
+            fromLists [] "a" [ "b", "c", "d", "e" ]
+
+        endSelected =
+            fromLists [ "a", "b", "c", "d" ] "e" []
     in
         describe "selectAt function"
-            [ mkSelectAtTest "select middle" 2 sList 
-                <| fromLists ["a", "b"] "c" ["d", "e"]
-            , mkSelectAtTest "select when start selected" 3 startSelected 
-                <| fromLists ["a", "b", "c"] "d" ["e"]
-            , mkSelectAtTest "select when end selected" 2 endSelected
-                <| fromLists ["a", "b"] "c" ["d", "e"]
+            [ mkSelectAtTest "select middle" 2 sList <|
+                fromLists [ "a", "b" ] "c" [ "d", "e" ]
+            , mkSelectAtTest "select when start selected" 3 startSelected <|
+                fromLists [ "a", "b", "c" ] "d" [ "e" ]
+            , mkSelectAtTest "select when end selected" 2 endSelected <|
+                fromLists [ "a", "b" ] "c" [ "d", "e" ]
             , mkSelectAtTest "singleton" 0 singleton singleton
             , mkSelectAtTest "invalid index" -1 sList sList
             ]
-        
+
+
 mkSelectAtTest : String -> Int -> SelectList a -> SelectList a -> Test
 mkSelectAtTest description element sList expected =
     test description <|
         \_ ->
             let
-                newSList = selectAt element sList
+                newSList =
+                    selectAt element sList
             in
                 Expect.equal newSList expected
-            
